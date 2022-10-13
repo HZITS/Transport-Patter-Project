a0cc8302d55c779d9713f320b5f0ce0f07a106cc
HZITS <46047375+HZITS@users.noreply.github.com>
2022-10-13 16:33:27 +0600

    Decorator setup(full for car)

--- src/CarSub/Manufacturer/Asian.java
+++ src/CarSub/Manufacturer/Asian.java
@@ -1,7 +1,24 @@
 package CarSub.Manufacturer;
 
 import CarSub.CarDecorator;
+import ConcreteObjects.Car;
 
 public class Asian extends CarDecorator {
 
+    public Asian(Car car) {
+        super(car);
+    }
+    @Override
+    public double getSpeed() {
+        return car.getSpeed()+13;
+    }
+    public double getKpl() {
+        return car.getKpl()*1.05;
+    }
+    public double getFuelPrice() {
+        return car.getFuelPrice();
+    }
+    public String getDescription(){
+        return car.getDescription()+"Asian ";
+    }
 }
--- src/CarSub/Manufacturer/Europe.java
+++ src/CarSub/Manufacturer/Europe.java
@@ -1,7 +1,24 @@
 package CarSub.Manufacturer;
 
 import CarSub.CarDecorator;
+import ConcreteObjects.Car;
 
 public class Europe extends CarDecorator {
 
+    public Europe(Car car) {
+        super(car);
+    }
+    @Override
+    public double getSpeed() {
+        return car.getSpeed()+10;
+    }
+    public double getKpl() {
+        return car.getKpl()*0.95;
+    }
+    public double getFuelPrice() {
+        return car.getFuelPrice();
+    }
+    public String getDescription(){
+        return car.getDescription()+"Europe ";
+    }
 }
