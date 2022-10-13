f47254863739189e000cb33ca051052ca1271fc4
HZITS <46047375+HZITS@users.noreply.github.com>
2022-10-13 17:51:26 +0600

    Fixing bug and stuff

--- src/CarFactory.java
+++ src/CarFactory.java
@@ -1,9 +1,48 @@
-import CarSub.BodyType.Sedan;
-import CarSub.CarDecorator;
 import ConcreteObjects.Car;
+
+import java.util.Scanner;
 
 public class CarFactory extends TransportFactory{
     public static Car createCar() {
-        return new Car();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Would you like to customize or straight to the point:");
+        System.out.println("1. Yes");
+        System.out.println("2. No");
+
+        System.out.print("\nEnter option (1-2): ");
+        Car car = null;
+        int option = sc.nextInt();
+        switch (option) {
+            case 1:
+                car = startFactory();
+                break;
+            case 2:
+                break;
+            default:
+                System.out.println("\nInput number invalid");
+                break;
+        }
+        return car;
     }
+        private static Car startFactory(){
+            System.out.println("Choose body type of car");
+            carBodyTypes();
+            System.out.println("Choose fuel type of car");
+            carFuelTypes();
+            System.out.println("Choose free or toll road");
+            carTollRoad();
+            System.out.println("Choose with driver or without");
+            carDriver();
+            return new Car();
+        }
+
+    private static void carFuelTypes() {
+    }
+
+    private static void carBodyTypes() {
+        
+    }
+
+}
 }
--- src/Navigator.java
+++ src/Navigator.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class Navigator {
+    Scanner sc = new Scanner(System.in);
+    TransportFactory tf = new TransportFactory();
+
+    public void start(){
+        Transport transport = null;
+        whileStatement:
+        while (true) {
+            System.out.println("\nWelcome to 3Gis!\nWould you like to travel by a plane or a car?\n");
+            System.out.println("1. Plane");
+            System.out.println("2. Car");
+            System.out.println("0. Exit");
+
+            System.out.print("\nEnter option (1-2): ");
+            int option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    transport = tf.createTransport(false);
+                    break;
+                case 2:
+                    transport = tf.createTransport(true);
+                    break;
+                case 0:
+                    System.out.println("\nAuf viedersehn!");
+                    break whileStatement;
+                default:
+                    System.out.println("\nInput number invalid");
+                    break;
+            }
+        }
+    }
 }
